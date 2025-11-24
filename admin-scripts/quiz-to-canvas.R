a7428fee9e74fb3f2ea5a0c6b375798fc947b73a
athos00 <26285561+athos00@users.noreply.github.com>
2025-11-24 16:05:40 -0500

    api lecture

--- admin-scripts/quiz-to-canvas.R
+++ admin-scripts/quiz-to-canvas.R
@@ -1,10 +1,10 @@
 # edit paths:
 library(tidyverse)
-# grades <- readxl::read_xlsx("~/Downloads/Grades(1).xlsx")
-
+grades <- readxl::read_xlsx("~/Downloads/Grades(2).xlsx")
+## Recall there are hidden columns in the data frame
 
 grades |>
   select(Student, ID, `SIS User ID`, `SIS Login ID`, Section,
-         quiz01, quiz02) |> # edit assignments 
-  mutate(across(c(quiz01, quiz02), ~replace_na(., 0))) |>
+         quiz03, quiz04, quiz05) |> # edit assignments 
+  # mutate(across(c(quiz03, quiz04, quiz05), ~replace_na(., 0))) |>
   write_csv(paste0("~/Downloads/upload323-", Sys.Date(), ".csv"))
