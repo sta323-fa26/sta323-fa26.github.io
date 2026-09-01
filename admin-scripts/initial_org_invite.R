49e1a9a5ddaa642cbc9c1b2ace23815e1d0aabbf
athos00 <26285561+athos00@users.noreply.github.com>
2026-09-01 11:20:34 -0400

    update Carrie's office hours

--- admin-scripts/initial_org_invite.R
+++ admin-scripts/initial_org_invite.R
@@ -40,3 +40,28 @@ org_create_assignment(
 )
 
 #ghclass::repo_add_user(repo = paste0("sta323-fa26/", repos), user = usernames, permission = "push")
+
+#
+## MAKE 
+##### FUTURE 
+######## LABS 
+
+this_org <- "sta323-fa26"
+assignment <- "lab-1"
+
+
+usernames <- ghclass::org_members(org = this_org)
+repos <- paste0(assignment, "-", usernames)
+existing_repos <- ghclass::org_repos(this_org, filter = assignment, full_repo = FALSE)
+
+indices <- !(repos %in% existing_repos)
+make_usernames <- usernames[indices]
+make_repos <- repos[indices]
+
+org_create_assignment(
+  org = this_org,
+  user = make_usernames,
+  repo = make_repos,
+  source_repo = paste0(this_org,"/", assignment),
+  private = TRUE
+)
