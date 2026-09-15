57b612d609c18d003ea0c2211609af1cf6f58ce9
athos00 <26285561+athos00@users.noreply.github.com>
2026-09-15 07:27:15 -0400

    soln

---
+++ notes/regexp-solutions.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+## Exercise 1
+
+emails <- readLines("https://sta323-fa26.github.io/data/emails.txt")
+
+str_match(emails, 
+                "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+") |>
+  data.frame(email = _) |>
+  drop_na() |> 
+  mutate(domain = str_extract(email, ("(?<=@)[^.*]+")))
+
+
+## Exercise 2
+
+files <- list.files("secret-messages/", pattern = ".txt")
+for (file in files) {
+  x <- readLines(paste0("secret-messages/", file))
+  str_extract(x, "sta323\\{.*\\}") |>
+    print()
+}
