48453511ec26309c9554cd72271c7c288d78f02f
athos00 <26285561+athos00@users.noreply.github.com>
2026-09-17 12:08:29 -0400

    updates & git FAQ

--- admin-scripts/download-artifacts.R
+++ admin-scripts/download-artifacts.R
@@ -3,7 +3,7 @@ library(ghclass)
 ##################################
 ### to download the html files ###
 ##################################
-repos_of_interest = ghclass::org_repos("sta323-fa26", filter="lab-1")
+repos_of_interest = ghclass::org_repos("sta323-fa26", filter="lab-2")
 ghclass::action_artifact_download(repos_of_interest,
                                   dir = paste0("~/Downloads/sta323-", Sys.Date()),
                                   overwrite = FALSE)
--- admin-scripts/quiz-to-canvas.R
+++ admin-scripts/quiz-to-canvas.R
@@ -1,10 +1,10 @@
 # edit paths:
 library(tidyverse)
-grades <- readxl::read_xlsx("~/Downloads/Grades(2).xlsx")
+grades <- readxl::read_xlsx("~/Downloads/grades(3).xlsx")
 ## Recall there are hidden columns in the data frame
 
 grades |>
   select(Student, ID, `SIS User ID`, `SIS Login ID`, Section,
-         quiz03, quiz04, quiz05) |> # edit assignments 
+         quiz01) |> # edit assignments 
   # mutate(across(c(quiz03, quiz04, quiz05), ~replace_na(., 0))) |>
   write_csv(paste0("~/Downloads/upload323-", Sys.Date(), ".csv"))
